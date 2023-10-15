ac99866e91fc7b1cfd87d65316a15ad2f2af98b1
Erica  Lira <104800676+ericapaiva@users.noreply.github.com>
2023-10-15 13:49:56 +0000

    Atividade dados

---
+++ Dados.js
@@ -0,0 +1,13 @@
+// Definindo um objeto para representar uma pessoa
+var pessoa = {
+  nome: "Danilo Lira", // tipo string
+  salario: 4500.5, // váriavel tipo number
+  idade: 30, // var tipo number
+  possuiDiploma: true, // tipo boolean
+};
+
+// Exibindo as informações do cadastro
+console.log("Nome:", pessoa.nome);
+console.log("Salário:", pessoa.salario);
+console.log("Idade:", pessoa.idade);
+console.log("Possui Diploma:", pessoa.possuiDiploma);
